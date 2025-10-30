438ccf0b2136faab948fd004049bfbe539a44975
muralievv <nmuraliev@gmail.com>
2025-10-30 19:12:53 +0600

    muraliev

---
+++ Homework1.py
@@ -0,0 +1,18 @@
+class Car:
+    def __init__(self, name, horsepower, weight):
+        self.name = name
+        self.horsepower = horsepower
+        self.weight = weight
+    def description(self):
+        return f"{self.name}, {self.horsepower} hp, {self.weight},kg"
+    def boost_horsepower(self):
+        return f"the {self.name} power increased by 50 hp"
+HyundaiPalisade = Car("Palisade", 291, 2050)
+SubaruForester = Car("Forester", 182, 1600)
+print(HyundaiPalisade.description())
+print(SubaruForester.description())
+print(HyundaiPalisade.boost_horsepower())
+print(SubaruForester.boost_horsepower())
+
+# print(f"{HyundaiPalisade.name}, {HyundaiPalisade.weight} kg, {HyundaiPalisade.horsepower}Hp")
+# print(SubaruForester.name)
